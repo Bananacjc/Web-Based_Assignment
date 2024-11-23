38c6aef0082065fcb53976e3eb9d19dee42cdd15
Bananacjc <jauchun040322@gmail.com>
2024-11-23 17:54:23 +0800

    Added code for: 1. Setting Timezone 2. Create session

---
+++ app/_base.php
@@ -0,0 +1,16 @@
+<?php
+
+// Basic PHP Setup
+date_default_timezone_set('Asia/Kuala_Lumpur'); // Set Timezone
+session_start(); // Enable session
+
+function redirect($url = null) {
+    $url ??= $_SERVER['REQUEST_URI'];
+    header("Location: ".$url);
+    exit();
+}
+
+// Redirect to an existing page
+redirect('page\\login.php');
+
+?>
