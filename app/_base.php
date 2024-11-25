1cb850004ea5050dc760e92ed3613dabb79c39e1
Bananacjc <jauchun040322@gmail.com>
2024-11-25 17:49:43 +0800

    Modified _base.php

--- app/_base.php
+++ app/_base.php
@@ -7,5 +7,38 @@
 // TODO
 
 date_default_timezone_set('Asia/Kuala_Lumpur');
+session_start();
+
+// ============================================================================
+// General Page Functions
+// ============================================================================
+
+// Is GET request?
+function is_get(): bool {
+    return $_SERVER['REQUEST_METHOD'] == 'GET';
+}
+
+// Is POST request?
+function is_post(): bool {
+    return $_SERVER['REQUEST_METHOD'] == 'POST';
+}
+
+// Obtain GET parameter
+function get($key, $value = null) {
+    $value = $_GET[$key] ?? $value;
+    return is_array($value) ? array_map('trim', $value) : trim($value);
+}
+
+// Obtain POST parameter
+function post($key, $value = null) {
+    $value = $_POST[$key] ?? $value;
+    return is_array($value) ? array_map('trim', $value) : trim($value);
+}
+
+// Obtain REQUEST (GET and POST) parameter
+function req($key, $value = null) {
+    $value = $_REQUEST[$key] ?? $value;
+    return is_array($value) ? array_map('trim', $value) : trim($value);
+}
 
 ?>
